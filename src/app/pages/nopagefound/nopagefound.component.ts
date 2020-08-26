ce5fae8ae8231b8aad47a462e7df6e1801b1ac54
Ariel <ariel@kyas.com.ar>
2020-08-25 22:36:41 -0300

    Implementar 404

--- src/app/pages/nopagefound/nopagefound.component.ts
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
   styleUrls: ['./nopagefound.component.css']
 })
-export class NopagefoundComponent implements OnInit {
+export class NopagefoundComponent{
 
-  constructor() { }
+  year;
 
-  ngOnInit(): void {
+  constructor() {
+    this.year = new Date().getFullYear();
   }
 
 }
